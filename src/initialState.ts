d9907c1bb778f7ff3a4a9c28f41711dee67e3432
akoval <akoval@playtika.com>
2020-07-13 00:11:31 +0300

    Add initial state

---
+++ src/initialState.ts
@@ -0,0 +1,8 @@
+export const INITIAL_STATE = [
+    { text: 'Call Dwight Schrute', label: 'Work', isDone: false },
+    { text: 'Learn redux', label: 'Work', isDone: true },
+    { text: 'Buy bananas', label: 'Family', isDone: false },
+    { text: 'Take out the trash', label: 'Family', isDone: true },
+    { text: 'Go for a walk', label: 'Personal', isDone: false },
+    { text: 'Do morning exercise', label: 'Personal', isDone: true },
+]
