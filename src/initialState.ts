6a2e368ccbd3d4ab2580f25b076eb74ecf8b158a
akoval <akoval@playtika.com>
2020-07-16 18:59:32 +0300

    Add redux to the app

--- src/initialState.ts
+++
@@ -1,8 +0,0 @@
-export const INITIAL_STATE = [
-    { text: 'Call Dwight Schrute', label: 'Work', isDone: false },
-    { text: 'Learn redux', label: 'Work', isDone: true },
-    { text: 'Buy bananas', label: 'Family', isDone: false },
-    { text: 'Take out the trash', label: 'Family', isDone: true },
-    { text: 'Go for a walk', label: 'Personal', isDone: false },
-    { text: 'Do morning exercise', label: 'Personal', isDone: true },
-]
